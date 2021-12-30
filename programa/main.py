451606ffee806907f2dcd70f07b4c38ee5ce6fd0
jonathanAca06 <jagg0612996@gmail.com>
2021-12-30 02:36:07 -0600

    "Formato"

--- programa/main.py
+++ programa/main.py
@@ -11,7 +11,6 @@ import argparse
 import requests
 import re
 
-
 class DataBase:
     def __init__(self):
         self.conexion = pymysql.connect(
@@ -22,7 +21,6 @@ class DataBase:
 
         )
         self.cursor = self.conexion.cursor()
-
         print("Conexion establecida exitosamente!")
 
     def select_muestra(self, id):
@@ -32,19 +30,11 @@ class DataBase:
             self.cursor.execute(query)
             muestra = self.cursor.fetchone()
             campos = []
-
             campos.append(muestra[0])
             campos.append(muestra[1])
             campos.append(muestra[2])
             campos.append(muestra[3])
             campos.append(muestra[4])
-
-        #    print("Id:", muestra[0])
-        #    print("File_name:", muestra[1])
-        #    print("Md5:", muestra[2])
-        #    print("Sha1:", muestra[3])
-        #    print("Sha256:", muestra[4])
-
         except Exception as e:
             raise
         return campos
@@ -57,12 +47,6 @@ class DataBase:
             list_muestras = []
             n = 0
             for muestra in muestras:
-                # print("Id:",muestra[0])
-                # print("File_name:",muestra[1])
-                # print("Md5:",muestra[2])
-                # print("Sha1:",muestra[3])
-                # print("Sha256:",muestra[4])
-                # print("____________________\n")
                 list_muestras.append(muestra[2])
         except Exception as e:
             raise
@@ -77,7 +61,6 @@ class DataBase:
            self.cursor.execute(query, valores)
            self.conexion.commit()
            print("GUARDAR LOS DATOS")
-           
        except Exception as e:
             raise 
 
@@ -90,7 +73,6 @@ class DataBase:
            self.cursor.execute(query, valores)
            self.conexion.commit()
            print("GUARDAR LOS DATOS")
-           
        except Exception as e:
             raise 
 
@@ -105,17 +87,6 @@ class DataBase:
     def close(self):
         self.conexion.close()
 
-
-
-
-
-
-# DB.select_all_muestras()
-time_duration = 15
-# print("Num muestras totales = ", auxs)
-
-a = 0
-# print("Muestras ejemplos:", aux_list[1])
 # Variables para la autentifacion de la API
 # De virus totak
 api_key = 'ed6515a99ff5bbfc2283bf82ee26f94aa45f1b3cad58fdc5726dc2d368b9f713'
@@ -149,7 +120,6 @@ familias_dic = {
     'T' : 'smsreg'
 }
 
-
 def temporizador():
     time.sleep(20)
 
@@ -157,14 +127,6 @@ def concatenar(url,hash):
     url_full = url + hash
     return url_full
 
-##
-# for list in aux_list:
-#    a = a + 1
-#   if(a < 10):
-#        print("Indice:", a)
-#        print("Prueba: ", list)
-#       temporizador()
-# #
 
 def main():
     DB = DataBase()
@@ -207,10 +169,6 @@ def main():
                     json.dump(data,file, indent=4)     
             temporizador() 
     DB.close()
-
-
-
-    
 
 if __name__ == '__main__':
     main()
