81f67748180449b621a81564509bbd54ed68abbe
jonathanAca06 <jagg0612996@gmail.com>
2021-12-30 00:38:30 -0600

    "Version modificada"

--- programa/main.py
+++ programa/main.py
@@ -12,8 +12,6 @@ import requests
 import re
 
 
-
-
 class DataBase:
     def __init__(self):
         self.conexion = pymysql.connect(
@@ -70,18 +68,33 @@ class DataBase:
             raise
         return list_muestras
 
-    def guardar_datos(self, id ):
+    def guardar_datos(self, muestra, resultado, familia, consulta_archivo):
        print("GUARDAR LOS DATOS")
+       # Sintaxis para insertar datos en msyql:
+       # INSERT INTO nombre_tabla (columna1, comluna2, ...) VALUES (valor1, valor2, ...)
+       valores = (muestra, resultado, familia, consulta_archivo)
+       query = "INSERT INTO `VirusTotal-00001`(muestra, resultado, familia, consulta_archivo) VALUES(%s, %s, %s, %s)"
+       try:
+           self.cursor.execute(query, valores)
+           self.conexion.commit()
+           
+       except Exception as e:
+            raise 
+
+    def close(self):
+        self.conexion.close()
+
+
 
 
 
 
 # DB.select_all_muestras()
 time_duration = 15
-#print("Num muestras totales = ", auxs)
+# print("Num muestras totales = ", auxs)
 
 a = 0
-#print("Muestras ejemplos:", aux_list[1])
+# print("Muestras ejemplos:", aux_list[1])
 # Variables para la autentifacion de la API
 # De virus totak
 api_key = 'ed6515a99ff5bbfc2283bf82ee26f94aa45f1b3cad58fdc5726dc2d368b9f713'
@@ -141,31 +154,32 @@ def main():
     aux_campos = DB.select_muestra(1)
     data = {}
     data['muestras'] = []
-    #print("Funcion->Id:", aux_campos[0])
+    # print("Funcion->Id:", aux_campos[0])
     aux_list = DB.select_all_muestras()
     print("Dato 1:", aux_list[0])
     aux_url= concatenar(url,aux_list[0])
     print("url: " , aux_url)
     response_json = requests.request("GET",aux_url, headers=headers) 
     consulta_dict = json.loads(response_json.content)
-    #print("Dicionario del query:\n", consulta_dict.keys())
+    # print("Dicionario del query:\n", consulta_dict.keys())
     
     familia_query = consulta_dict['data'][0]['attributes']['popular_threat_classification']['suggested_threat_label']
     print("Diccionario version 1: ", familia_query)
     
     for key  in  familias_dic:
         if re.search(familias_dic[key],familia_query):
-            #print('coincidecia o tiene una subcadena')
+            # print('coincidecia o tiene una subcadena')
             data['muestras'].append({
                 'id'          : aux_list[0],
-                'virus total' : familia_query,
+                'clasificacion' : familia_query,
                 'familia'     : familias_dic[key].upper(),
                 'fecha'       : str(fecha)})
             file_name = str(fecha) + '.json'
             with open(file_name,'w') as file:
                 json.dump(data,file, indent=4)
+            DB.guardar_datos(aux_list[0], familia_query, familias_dic[key].upper(), response_json.content)
     
-        
+    DB.close()
 
 
 
