a424e37144d23587252706b80ca843a60e57d983
jonathanAca06 <jagg0612996@gmail.com>
2022-01-07 18:46:38 -0600

    "Actualización "

--- programa/main.py
+++ programa/main.py
@@ -2,6 +2,7 @@
 # Programa para el uso de la api visrus total y la clasificaioón
 # muestras (5540)
 ##
+from pathlib import Path
 import datetime
 import time
 import pymysql
@@ -10,6 +11,7 @@ import json
 import argparse
 import requests
 import re
+import os
 
 class DataBase:
     def __init__(self):
@@ -38,9 +40,77 @@ class DataBase:
         except Exception as e:
             raise
         return campos
+   
+    #Seleccion de datos con muestras con limites: de n a n
+    def seleccion_muestra_n_n(self, tabla, valor_inical, valor_final):
+        limites = (valor_inical, valor_final)
+        if tabla == 0:
+            query = 'SELECT id, file_name, md5, sha1, sha256 FROM `Drebin-00000` LIMIT %s,%s'
+        elif tabla == 1:
+            query = 'SELECT id, file_name, md5, sha1, sha256 FROM `Drebin-00001` LIMIT %s,%s'
+        elif tabla == 2:
+            query = 'SELECT id, file_name, md5, sha1, sha256 FROM `Drebin-00002` LIMIT %s,%s'
+        elif tabla == 3: 
+            query = 'SELECT id, file_name, md5, sha1, sha256 FROM `Drebin-00003` LIMIT %s,%s'
+        elif tabla == 4:
+            query = 'SELECT id, file_name, md5, sha1, sha256 FROM `Drebin-00004` LIMIT %s,%s'
+        else:
+            query = 'SELECT id, file_name, md5, sha1, sha256 FROM `Drebin-00005` LIMIT %s,%s'
 
-    def select_all_muestras(self):
-        query = 'SELECT id, file_name, md5, sha1, sha256 FROM `Drebin-00000` '
+        try:
+            self.cursor.execute(query,limites)
+            muestras = self.cursor.fetchall()
+            lista_muestra = []
+            for muestra in muestras:
+                lista_muestra.append(muestra[2])
+        except Exception as e:
+            raise
+        
+        return lista_muestra 
+
+    #Seleccion de datos con muestras con limites:
+    #Primeros 100 muestras.
+    def seleccion_muestras_0_100(self,seleccion):
+        
+        if seleccion == 0:
+            query = 'SELECT id, file_name, md5, sha1, sha256 FROM `Drebin-00000` LIMIT 100'
+        elif seleccion == 1:
+            query = 'SELECT id, file_name, md5, sha1, sha256 FROM `Drebin-00001` LIMIT 100'
+        elif seleccion == 2:
+            query = 'SELECT id, file_name, md5, sha1, sha256 FROM `Drebin-00002` LIMIT 100'
+        elif seleccion == 3: 
+            query = 'SELECT id, file_name, md5, sha1, sha256 FROM `Drebin-00003` LIMIT 100'
+        elif seleccion == 4:
+            query = 'SELECT id, file_name, md5, sha1, sha256 FROM `Drebin-00004` LIMIT 100'
+        else:
+            query = 'SELECT id, file_name, md5, sha1, sha256 FROM `Drebin-00005` LIMIT 100'
+
+        try:
+            self.cursor.execute(query)
+            muestras = self.cursor.fetchall()
+            lista_muestra = []
+            for muestra in muestras:
+                lista_muestra.append(muestra[2])
+        except Exception as e:
+            raise
+        
+        return lista_muestra
+
+    #Consulta de todos los registros:
+    def select_all_muestras(self, seleccion):
+        if seleccion == 0:
+            query = 'SELECT id, file_name, md5, sha1, sha256 FROM `Drebin-00000` '
+        elif seleccion == 1:
+            query = 'SELECT id, file_name, md5, sha1, sha256 FROM `Drebin-00001` '
+        elif seleccion == 2:
+            query = 'SELECT id, file_name, md5, sha1, sha256 FROM `Drebin-00002` '
+        elif seleccion == 3:
+            query = 'SELECT id, file_name, md5, sha1, sha256 FROM `Drebin-00003` '
+        elif seleccion == 4:
+            query = 'SELECT id, file_name, md5, sha1, sha256 FROM `Drebin-00004` '
+        else:
+            query = 'SELECT id, file_name, md5, sha1, sha256 FROM `Drebin-00005` '
+
         try:
             self.cursor.execute(query)
             muestras = self.cursor.fetchall()
@@ -52,32 +122,61 @@ class DataBase:
             raise
         return list_muestras
 
-    def guardar_datos_clasificacion_familias(self, muestra, resultado, familia, consulta_archivo):
+    def mostrar_datos_clasificados(self, id):
+        print("Mostrar datos")
+        query = 'SELECT muestra, resultado, familia, consulta_archivo, sha256 FROM `Drebin-00000` WHERE id = {}'.format(
+            id)
+        try:
+            self.cursor.execute(query)
+            muestra = self.cursor.fetchone()
+            campos = []
+            campos.append(muestra[0])
+            campos.append(muestra[1])
+            campos.append(muestra[2])
+            campos.append(muestra[3])
+            campos.append(muestra[4])
+        except Exception as e:
+            raise
+        return campos
+
+    def guardar_datos_clasificacion_familias(self,num_tabla ,muestra, resultado, familia, consulta_archivo):
        # Sintaxis para insertar datos en msyql:
        # INSERT INTO nombre_tabla (columna1, comluna2, ...) VALUES (valor1, valor2, ...)
        valores = (muestra, resultado, familia, consulta_archivo)
-       query = "INSERT INTO `VirusTotal-00001`(muestra, resultado, familia, consulta_archivo) VALUES(%s, %s, %s, %s)"
+       if num_tabla == 0:
+           query = "INSERT INTO `VirusTotal-00000`(muestra, resultado, familia, consulta_json) VALUES(%s, %s, %s, %s)"
+       elif num_tabla == 1:
+           query = "INSERT INTO `VirusTotal-00001`(muestra, resultado, familia, consulta_json) VALUES(%s, %s, %s, %s)"
+       elif num_tabla == 2:
+           query = "INSERT INTO `VirusTotal-00002`(muestra, resultado, familia, consulta_json) VALUES(%s, %s, %s, %s)"
+       elif num_tabla == 3:
+           query = "INSERT INTO `VirusTotal-00003`(muestra, resultado, familia, consulta_json) VALUES(%s, %s, %s, %s)"
+       elif num_tabla == 4:
+           query = "INSERT INTO `VirusTotal-00004`(muestra, resultado, familia, consulta_json) VALUES(%s, %s, %s, %s)" 
+       else:
+           query = "INSERT INTO `VirusTotal-00005`(muestra, resultado, familia, consulta_json) VALUES(%s, %s, %s, %s)" 
+       
        try:
            self.cursor.execute(query, valores)
            self.conexion.commit()
-           print("GUARDAR LOS DATOS")
+           print("DATOS GUARDADOS")
        except Exception as e:
             raise 
 
-    def guardar_datos_clasificacion_diferentes(self, muestra, clasificacion, consulta_archivo):
-       # Sintaxis para insertar datos en msyql:
-       # INSERT INTO nombre_tabla (columna1, comluna2, ...) VALUES (valor1, valor2, ...)
-       valores = (muestra, clasificacion,  consulta_archivo)
-       query = "INSERT INTO `VirusTotalDiferente-00001`(muestra, clasificacion, consulta_archivo) VALUES(%s, %s, %s)"
-       try:
-           self.cursor.execute(query, valores)
-           self.conexion.commit()
-           print("GUARDAR LOS DATOS")
-       except Exception as e:
-            raise 
 
-    def elimanar_registros(self, nombre_tabla):
-        query = "truncate `VirusTotal-00001`"
+    def elimanar_registros(self, num_tabla):
+        if num_tabla == 0:
+            query = "truncate `VirusTotal-00000`"
+        elif num_tabla == 1:
+            query = "truncate `VirusTotal-00001`"
+        elif num_tabla == 2:
+            query = "truncate `VirusTotal-00002`" 
+        elif num_tabla == 3:
+            query = "truncate `VirusTotal-00003`" 
+        elif num_tabla == 4:
+            query = "truncate `VirusTotal-00004`" 
+        else:
+            query = "truncate `VirusTotal-00005`"  
         try:
             self.cursor.execute(query)
             self.conexion.commit()
@@ -86,6 +185,9 @@ class DataBase:
 
     def close(self):
         self.conexion.close()
+
+#Limpiar terminal:
+clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 
 # Variables para la autentifacion de la API
 # De virus totak
@@ -96,7 +198,7 @@ headers = {
     "Accept": "application/json",
     "x-apikey": api_key
 }
-
+#En conocer otras abreviaciones a las 
 familias_dic = {
     'A' : 'fakeinstaller',
     'B' : 'droidkungfu',
@@ -128,47 +230,101 @@ def concatenar(url,hash):
     return url_full
 
 
+
+def generar_archivo_bitacora():
+    print("Realiza la bitacoras de las consultas que se hicieron")
+
 def main():
     DB = DataBase()
     fecha = datetime.datetime.today()
-    # DB.select_muestra(1)
     aux_list = []
-    aux_campos = []
-    aux_campos = DB.select_muestra(1)
-    data = {}
-    data['muestras'] = []
-    # print("Funcion->Id:", aux_campos[0])
-    aux_list = DB.select_all_muestras()
     
     a = 0
+    datos = {}
+    #aux_campos = []
+    #aux_campos = DB.select_muestra(1)
+    #data['muestras'] = []
+    # print("Funcion->Id:", aux_campos[0])
     
-    for list in aux_list:
-        a = a + 1
-        if(a < 10):
-            print("Contador: ", a)
-            print("Muestra: ", list)
-            aux_url= concatenar(url,list)
-            print("url: " , aux_url)
-            response_json = requests.request("GET",aux_url, headers=headers) 
-            consulta_dict = json.loads(response_json.content)
-            familia_query = consulta_dict['data'][0]['attributes']['popular_threat_classification']['suggested_threat_label']
-            print("Diccionario version 1: ", familia_query)
-            for key  in  familias_dic:
-                if re.search(familias_dic[key],familia_query):
-                    DB.guardar_datos_clasificacion_familias(list, familia_query, familias_dic[key].upper(), response_json.content)
-                else:
-                    print("otro clasificacipne")
-                    DB.guardar_datos(list, familia_query, response_json.content) 
-                data['muestras'].append({
-                    'id'          : list,
-                        'clasificacion' : familia_query,
-                        'familia'     : familias_dic[key].upper(),
-                        'fecha'       : str(fecha)})
-                file_name = str(fecha) + '.json'
-                with open(file_name,'w') as file:
-                    json.dump(data,file, indent=4)     
-            temporizador() 
-    DB.close()
+    
+    opc = 0
+    while opc != 4 :
+            print("Opciones: \n 1.- Funcion de realizar las consultas primero 100 \n 2.- Funcion de realizar las consultas de un punto inical a final \n 3.- Limpiar la tabla de las consultas \n 4.- Salir ")
+            opc = input("Seleccione una opcion: ")
+            opc = int(opc)
+            if(opc == 1):
+                print("Opcion 1")
+                tabla = input("Seleccione una tabla de 0 - 5: ")
+                tabla = int(tabla)
+                aux_list = DB.seleccion_muestras_0_100(tabla)
+            
+                for list in aux_list:
+                    a = a + 1
+                    familia = True
+                    print("Contador: ", a)
+                    print("Muestra: ", list)
+                    aux_url= concatenar(url,list)
+                    print("url: " , aux_url)
+                    response_json = requests.request("GET",aux_url, headers=headers)
+                    consulta_dict = json.loads(response_json.content)
+                    familia_query = consulta_dict['data'][0]['attributes']['popular_threat_classification']['suggested_threat_label']
+                    print("Diccionario version 1: ", familia_query)
+                    for key in familias_dic:
+                        if re.search(familias_dic[key],familia_query):
+                            familia = False
+                            print(familias_dic[key].upper())
+                            DB.guardar_datos_clasificacion_familias(tabla,list, familia_query, familias_dic[key].upper(), response_json.content) 
+                    if familia == True:
+                        DB.guardar_datos_clasificacion_familias(tabla,list, familia_query, "NINGUNA", response_json.content)
+                        print("NINGUNA") 
+                    temporizador()                       
+                print("fin")
+            
+            elif(opc == 2):
+                print("Opcion 2")
+                tabla = input("Seleccione una tabla de 0 - 5: ")
+                tabla = int(tabla)
+                inicial = input("Inicio: ")
+                inicial = int(inicial) 
+                final = input("Final: ")
+                final = int(final)
+                aux_lista = DB.seleccion_muestras_n_n(tabla,inicial,final)
+                
+                for lista in aux_lista:
+                    a = a + 1
+                    familia = True
+                    print("Contador: ", a)
+                    print("Muestra: ", lista)
+                    aux_url= concatenar(url,lista)
+                    print("url: " , aux_url)
+                    response_json = requests.request("GET",aux_url, headers=headers)
+                    consulta_dict = json.loads(response_json.content)
+                    familia_query = consulta_dict['data'][0]['attributes']['popular_threat_classification']['suggested_threat_label']
+                    print("Diccionario version 1: ", familia_query)
+                    for key in familias_dic:
+                        if re.search(familias_dic[key],familia_query):
+                            familia = False
+                            print(familias_dic[key].upper())
+                            DB.guardar_datos_clasificacion_familias(tabla,lista, familia_query, familias_dic[key].upper(), response_json.content) 
+                    if familia == True:
+                        DB.guardar_datos_clasificacion_familias(tabla,lista, familia_query, "NINGUNA", response_json.content)
+                        print("NINGUNA") 
+                    temporizador()
+
+            
+            elif(opc == 3):
+                print("Opcion 3")
+                tabla = input("Seleccione una tabla de 0 - 5: ")
+                tabla = int(tabla)
+                DB.elimanar_registros(tabla)
+
+            elif(opc == 4):
+                print("Opcion 4: Salir")
+            else:
+                print("No hay opcion para ese dato")
+            #clearConsole()
+
+    
 
 if __name__ == '__main__':
     main()
